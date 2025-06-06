a66ee067200228c0e0e5dc3a725322b57cbbe24e
johnwgomez <johnwgomez10@gmail.com>
2025-06-06 14:53:53 -0700

    cypress config file add to root

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+// cypress.config.ts
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false
+  }
+})
